c41192d95a1aea8b8d82f5053f5e3d522c966102
Austin McCartney <amccartney@lanl.gov>
2016-10-23 20:05:58 -0600

    metaconfigure

---
+++ src/header-utilities/test/slurpFileToMemory.test.cpp
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+
+#include "header-utilities.hpp"
+
+extern int testNumber;
+
+using namespace njoy::utility;
+
+SCENARIO("slurpFileToMemory"){
+  GIVEN("a path to a file with known contents"){
+    std::string path = "./reference_file.txt";
+    std::string contents =
+      "Yesterday, all my troubles seemed so far away\n"
+      "Now it looks as though they’re here to stay\n"
+      "Oh, I believe in yesterday\n";
+    WHEN("the file at the specified path is slurped to memory"){
+      auto trial = slurpFileToMemory( path );
+      THEN("the returned string will match the known contents"){
+        REQUIRE( contents == trial );
+      }
+    }
+  }
+  GIVEN("a path to a non-existent file"){
+    THEN("the slurpFileToMemory function will throw"){
+      std::string path = "foobarbaz.xyz";
+      REQUIRE_THROWS( slurpFileToMemory( path ) );
+    }
+  }
+}

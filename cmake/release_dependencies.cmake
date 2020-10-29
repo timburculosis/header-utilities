a302e7287b0b60bfe3d52d04da32c267e5c53a04
Nathan Gibson <ngibson@lanl.gov>
2020-10-29 10:31:17 -0600

    Changed order of release dependencies.

--- cmake/release_dependencies.cmake
+++ cmake/release_dependencies.cmake
@@ -5,11 +5,6 @@ include( FetchContent )
 # Declare project dependencies
 #######################################################################
 
-FetchContent_Declare( spdlog
-    GIT_REPOSITORY  https://github.com/gabime/spdlog
-    GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
-    )
-
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  https://github.com/njoy/catch-adapter
     GIT_TAG         fb84b82ebf7a4789aa43cea560680cf745c6ee4f
@@ -20,12 +15,17 @@ FetchContent_Declare( Log
     GIT_TAG         52962b7796afe37ef1d8f7edb4bf9ecb1b868d15
     )
 
+FetchContent_Declare( spdlog
+    GIT_REPOSITORY  https://github.com/gabime/spdlog
+    GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
+    )
+
 #######################################################################
 # Load dependencies
 #######################################################################
 
 FetchContent_MakeAvailable(
-    spdlog
     catch-adapter
     Log
+    spdlog
     )
